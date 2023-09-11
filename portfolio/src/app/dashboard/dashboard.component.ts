e4d1b79accf9e589f5bdaeab641b95da35c8a43c
Nishit <nishit.m@microfocus.com>
2023-09-11 13:59:14 +0530

    Added typed.js for dashboard page

--- portfolio/src/app/dashboard/dashboard.component.ts
+++ portfolio/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.getTypingText();
+  }
+
+  private getTypingText(){
+    const typed = new Typed('.typing-text', {
+      strings: ['frontend development', 'backend development', 'web designing', 'Full Stack development', 'web development'],
+      loop: true,
+      typeSpeed: 50,
+      backSpeed: 25,
+      backDelay: 500,
+    });
+  }  
 
 }
