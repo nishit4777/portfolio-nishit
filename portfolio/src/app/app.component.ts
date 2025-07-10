f2ab1d69ec3fd30e7cb82c4ee15cab931fdb7820
nishitm <nishitm@opentext.com>
2025-07-10 15:44:04 +0530

    Added Skills page to portfolio

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    standalone: true,
+    imports: [
+        CommonModule,
+        NavigationComponent,
+        DashboardComponent,
+        AboutComponent,
+        ContactComponent,
+        ExperienceComponent,
+        SkillsComponent
+    ],
 })
 export class AppComponent {
   public title = 'portfolio';
--- portfolio/src/app/navigation/navigation.component.ts
+++ portfolio/src/app/navigation/navigation.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.css'],
-    standalone: true
+    standalone: true,
+    imports:[
+      CommonModule
+    ]
 })
 export class NavigationComponent {
 
